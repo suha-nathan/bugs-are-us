c681ee2478079052db69f5601ef4c5a0832d1313
suhanathan <suhasini_95@hotmail.com>
2021-03-20 23:28:53 +0800

    setup JWT auth and integrated with React

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,16 @@ const express = require('express')
 const app = express()
 require('./lib/mongoose.config')
 const cors = require('cors')
-
+const authChecker = require('./lib/authChecker')
 
 app.use(express.json())
 app.use(cors())
 
 
-app.use('/user', require('./routes/user.route'))
-app.use('/bug', require('./routes/bug.route'))
+app.use('/auth', require('./routes/auth.route'))
+app.use('/user', authChecker, require('./routes/user.route'))
+app.use('/bug',authChecker, require('./routes/bug.route'))
+
 
 
 app.listen(process.env.PORT, () => {
--- client/src/components/SignupInputCol.js
+++ client/src/components/SignupInputCol.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Col, Form, Row} from "react-bootstrap";
 
-const SignupInputCol = ({name, placeholder, size, isTextarea}) => {
+const SignupInputCol = ({name, placeholder, size, isTextarea,setUserInfo}) => {
 
     return (
         <>
             <Col md={size} className="signup-input-col d-flex flex-column justify-content-center position-relative">
-                <Form.Control name={name} placeholder={placeholder} as={isTextarea && "textarea"} rows={3}/>
+                <Form.Control
+                    name={name}
+                    placeholder={placeholder}
+                    onChange={(e)=>setUserInfo(prevState=>({...prevState, ...{[e.target.name]: e.target.value}}))}
+                    as={isTextarea && "textarea"}
+                    rows={3}/>
                 <p className="signup-input-col__error-message text-left my-1">Error Message</p>
             </Col>
 
