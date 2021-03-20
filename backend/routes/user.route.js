7533c80ea2f5fa05dff3346e3c6f0d2a7ede7f85
loganlek@outlook.com <loganlek@outlook.com>
2021-03-20 17:11:54 +0800

    added signin & bug route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,35 +1,44 @@
 const router = require('express').Router()
 const User = require('../model/user.model')
+const bcrypt = require('bcrypt')
 
 router.post("/signup", async (req, res) => {
     try{
         let {firstName, lastName, email, password} = req.body
-        console.log(req.body)
+
         const saveObj = {
             firstName,
             lastName,
             email,
-            password //try commenting me
+            password //: hashedPassword //try commenting me
         }
         const user = new User(saveObj)
+
         await user.save()
         res.send(user) //try logging req.body
+
     }catch (e) {
         res.status(400).json({ message : "Registration Failed, try again"})
         console.log(e)
     }
 })
 
-
-router.get("/test", async (req, res) => {
+router.post("/signin", async(req, res) => {
     try{
-        res.send('hello from test')
-    }catch (e) {
-        res.status(400).json({ message : "Registration Failed, try again"})
-        console.log(e)
+        let {email, password} = req.body
+        console.log(req.body)
+        const user = await User.findOne({ email})
+        if(user){
+            let isMatch = await bcrypt.compare(password, user.password)
+            if(isMatch){
+                res.status(200).json({ message : "logged in "})
+            }
+            res.status(400).json({ message : "Invalid Username or Password, try again"})
+        }
+    }catch(e){
+        res.status(400).json({ message: "Invalid Username or Password, try again"})
     }
 })
-
 
 
 
