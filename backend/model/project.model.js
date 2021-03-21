dd154630706e4071a5481879bc539dffc4c491c5
loganlek@outlook.com <loganlek@outlook.com>
2021-03-21 21:39:22 +0800

    working on editing comment route

--- backend/model/project.model.js
+++ backend/model/project.model.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const projectSchema = new Schema [{
+const projectSchema = new Schema ({
     title: { type: String, required: true},
     description: { type: String, required: true},
     categories: [],
-    bugs: [{ type: Schema.Types.ObjectId}],
-    members: [{ type: Schema.Types.ObjectId}],
-    projectLead: [{ type: Schema.Types.ObjectId}]
-}]
+    bugs: [{ type: Schema.Types.ObjectId, ref: 'Bug'}],
+    members: [{ type: Schema.Types.ObjectId, ref: 'User'}],
+    projectLead: [{ type: Schema.Types.ObjectId, ref: 'User'}]
+})
 
 
 module.exports = mongoose.model("Project", projectSchema)
