88cf550e1a88489e3aafc77d4ef5b86639c745ec
loganlek@outlook.com <loganlek@outlook.com>
2021-03-24 16:27:41 +0800

    Upvote Feature Route Implemented

--- backend/routes/upvote.route.js
+++ backend/routes/upvote.route.js
@@ -1,15 +1,22 @@
 const router = require('express').Router()
 const Bug = require('../model/bug.model')
 
-router.put('/edit/:id', async(req, res) => {
-    try{
-        const upVote = await Bug.findById(req.params.id).exec()
-        const foundUpvote = upVote.upVotes.findIndex( upvote => upvote._id == req.body.)
 
-        // console.log(req.body)
+router.put('/update/:id', async(req, res) => {
+    try{
+        const bug = await Bug.findById(req.params.id).exec()
+
+        if(bug.upVotes.includes(req.body.userId)){
+            throw "You have already Upvoted"
+        }
+        bug.upVotes.push(req.body.userId)
+        console.log(bug.upVotes)
+
+        await bug.save()
+
         res.status(200).json({ message: "Upvote succeeded"})
     }catch(e){
-        res.status(400).json({ message: "Upvote failed"})
+        res.status(400).json({ message: e || "Upvote failed"})
     }
 })
 
