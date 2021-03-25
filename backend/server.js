dacadea85a78a7486c4e1c3b4346157d242f12ff
Isaac Yong <isaac.yongty@gmail.com>
2021-03-25 14:52:27 +0800

    added path to build

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,11 @@ require('./lib/mongoose.config')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const authChecker = require('./lib/authChecker')
+const path = require('path')
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
+app.user(express.static(path.join(__dirname, './build')))
 // app.use(express.json())
 app.use(cors())
 
@@ -20,6 +21,15 @@ app.use('/project' ,authChecker, require('./routes/project.route'))
 app.use('/comment', authChecker, require('./routes/comment.route'))
 app.use('/upvote', authChecker, require('./routes/upvote.route'))
 
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build/index.html'))
+})
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'Route Not Found'
+    })
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`running on ${process.env.PORT}`)
